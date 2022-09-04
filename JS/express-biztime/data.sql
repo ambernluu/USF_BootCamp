db7ec486bfdffdc6d043d738fde3131691d877d4
ambernluu <85398285+ambernluu@users.noreply.github.com>
2022-09-03 21:27:06 -0400

    Add files via upload

--- JS/express-biztime/data.sql
+++ JS/express-biztime/data.sql
@@ -1,7 +1,13 @@
+
+DROP DATABASE biztime;
+CREATE DATABASE biztime;
 \c biztime
+
 
 DROP TABLE IF EXISTS invoices;
 DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS industries;
+DROP TABLE IF EXISTS industries_companies;
 
 CREATE TABLE companies (
     code text PRIMARY KEY,
@@ -19,8 +25,19 @@ CREATE TABLE invoices (
     CONSTRAINT invoices_amt_check CHECK ((amt > (0)::double precision))
 );
 
+CREATE TABLE industries (
+    code text PRIMARY KEY,
+    industry text NOT NULL
+);
+
+CREATE TABLE industries_companies (
+    industry_code TEXT REFERENCES industries ON DELETE CASCADE, 
+    company_code TEXT REFERENCES companies ON DELETE CASCADE  
+);
+
 INSERT INTO companies
   VALUES ('apple', 'Apple Computer', 'Maker of OSX.'),
+         ('samsung', 'Samsung', 'Electronics'),
          ('ibm', 'IBM', 'Big blue.');
 
 INSERT INTO invoices (comp_Code, amt, paid, paid_date)
@@ -28,3 +45,15 @@ INSERT INTO invoices (comp_Code, amt, paid, paid_date)
          ('apple', 200, false, null),
          ('apple', 300, true, '2018-01-01'),
          ('ibm', 400, false, null);
+
+INSERT INTO industries
+  VALUES ('comp', 'Computer and technology'),
+         ('cons', 'Construction'),
+         ('app', 'Appliances'),
+         ('edu', 'Education');
+
+INSERT INTO industries_companies
+  VALUES ('comp', 'ibm'),
+         ('comp', 'samsung'),
+         ('app', 'samsung'), 
+         ('comp', 'apple');
